a9828a2014f28198311bbad2059b09cf5a1f8476
Doaa Mosallam <Doaamosalam9@gmail.com>
2025-03-16 05:27:40 +0200

    solving problem from book How to program c++

---
+++ HowToProgramC++/Chapter_2/chapterTwo.h
@@ -0,0 +1,30 @@
+//
+// Created by DoaaM on 3/16/2025.
+//
+
+#ifndef CHAPTERTWO_H
+#define CHAPTERTWO_H
+#include <iostream>
+
+using namespace std;
+class ChapterTwo {
+public:
+    void solving() {
+        // What, if anything, prints when each of the following C++ statements is performed? If nothing prints, then answer “nothing.” Assume x = 2 and y = 3.
+        int x =2;
+        int y =3;
+        int z;
+        cout << x;
+        cout << x + x;
+        cout << "x=";
+        cout << "x = " << x;
+        cout << x + y << " = " << y + x;
+        z = x + y;
+        cin >> x >> y;
+        cout << "x + y = " << x + y;
+        cout << "\n";
+        //===============
+    }
+};
+
+#endif //CHAPTERTWO_H
