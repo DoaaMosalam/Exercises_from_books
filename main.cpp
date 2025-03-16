1c4efc7f7a6ae4b48e1691c2138248e39ca694d9
Doaa Mosallam <Doaamosalam9@gmail.com>
2025-03-16 05:24:03 +0200

    solving problem from book How to program c++

--- main.cpp
+++ main.cpp
@@ -13,6 +13,19 @@ using namespace  std;
 
 int main()
 {
+
+ // What, if anything, prints when each of the following C++ statements is performed? If nothing prints, then answer “nothing.” Assume x = 2 and y = 3.
+ int x =2;
+ int y =3;
+cout << x;
+  cout << x + x;
+    cout << "x=";
+     cout << "x = " << x;
+    cout << x + y << " = " << y + x;
+    // z = x + y;
+     // cin >> x >> y;
+     // cout << "x + y = " << x + y;
+    // cout << "\n";
     //========================================================================================
     /*
      * solving problem from Object Oriented Programming in C++ book
