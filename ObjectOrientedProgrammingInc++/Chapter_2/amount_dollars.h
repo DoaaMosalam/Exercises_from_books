17c3755576c15a358de2673c0b89aeb40ce6550e
Doaa Mosallam <Doaamosalam9@gmail.com>
2025-03-16 04:58:42 +0200

    solving problem from book How to program c++

--- ObjectOrientedProgrammingInc++/Chapter_2/amount_dollars.h
+++ ObjectOrientedProgrammingInc++/Chapter_2/amount_dollars.h
@@ -13,18 +13,20 @@ value converted to these four other monetary units.
  */
 #endif //AMOUNT_DOLLARS_H
 using namespace std;
+class amount_dollars {
+public:
+    void anoumt_in_dollars() {
+        float amount;
+        float pound = 1.487;
+        float franc = 0.172;
+        float deutschemark = 0.584;
+        float yen = 0.00955;
 
-void anoumt_in_dollars() {
-    float amount;
-    float pound = 1.487;
-    float franc = 0.172;
-    float deutschemark = 0.584;
-    float yen = 0.00955;
-
-    cout<<"Enter the amount in dollars: ";
-    cin>>amount;
-    cout<<"your amount is: " <<amount << " The amount is pound is: " << amount * pound <<endl;
-    cout<<"your amount is: " <<amount << " The amount is franc is: " << amount * franc <<endl;
-    cout<<"your amount is: " <<amount << " The amount is deutschemark is: " << amount * deutschemark <<endl;
-    cout<<"your amount is: " <<amount << " The amount is yen is: " << amount * yen <<endl;
+        cout<<"Enter the amount in dollars: ";
+        cin>>amount;
+        cout<<"your amount is: " <<amount << " The amount is pound is: " << amount * pound <<endl;
+        cout<<"your amount is: " <<amount << " The amount is franc is: " << amount * franc <<endl;
+        cout<<"your amount is: " <<amount << " The amount is deutschemark is: " << amount * deutschemark <<endl;
+        cout<<"your amount is: " <<amount << " The amount is yen is: " << amount * yen <<endl;
+    }
 }
